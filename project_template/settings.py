a29e6c1cc18c160ae6d8df44e7bfe55560ac0961
Ryan Nguyen <u0877388@utah.edu>
2026-07-22 03:11:21 +0000

    Fixed login and regester issue (error 403)

--- project_template/settings.py
+++ project_template/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = "django-insecure-1lg++l_25!vbv=#f=o@wvw4im5fdqs9q#3%3pdu@tubd^^+woq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "https://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://127.0.0.1:8000",
+]
 
 
 # Application definition
